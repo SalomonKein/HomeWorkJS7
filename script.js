6a8284c5842cc2a23f32f50ae96992d1e30fde78
SalomonKein <61635747+SalomonKein@users.noreply.github.com>
2020-05-29 10:59:45 +0300

    add

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
 function createTreeOl(arrIn) {
 
-    // function createUl(arr){
-    //     li = document.createElement('li');
-    //     ol.append(li);
-    //     let ul = document.createElement(`ul`);
-    //     li.append(ul);
-    //     for (let elem of arr) {
-    //         if (typeof(elem) !== "object") {
-    //         let li = document.createElement('li');
-    //         ul.append(li);
-    //         li.append(elem);}
-    //          if (typeof(elem) == "object"){
-                
-    //              createUl(elem)
-    //             };
-        
 
-    //         };
-    //     };
     let ol = document.createElement(`ol`);
 
     body.append(ol);
@@ -28,41 +11,27 @@ function createTreeOl(arrIn) {
             ol.append(li);
             li.append(elem);
         };
-          if (typeof(elem) == "object") {
-            let arr=elem;
-              li = document.createElement('li');
-              ol.append(li);
-              let ul = document.createElement(`ul`);
-              li.append(ul);
-              for (let elem of arr) {
-                  
-                  if (typeof(elem) !== "object") {
-                  li = document.createElement('li');
-                  ul.append(li);
-                  li.append(elem);}
-                   if (typeof(elem) == "object"){
-                      
-                    let arr=elem;
+        if (typeof(elem) == "object") {
+            let arr = elem;
+            li = document.createElement('li');
+            ol.append(li);
+            let ul = document.createElement(`ul`);
+            li.append(ul);
+            for (let elem of arr) {
+                if (typeof(elem) !== "object") {
+                    li = document.createElement('li');
+                    ul.append(li);
+                    li.append(elem);
+                }
+                if (typeof(elem) == "object") {
+                    let arr = elem;
                     li = document.createElement('li');
                     ol.append(li);
                     let ul = document.createElement(`ul`);
                     li.append(ul);
-                    for (let elem of arr) {
-                        
-                        if (typeof(elem) !== "object") {
-                        li = document.createElement('li');
-                        ul.append(li);
-                        li.append(elem);}
-                         if (typeof(elem) == "object"){
-                            
-                             createUl(elem)
-                            };}
-                      };
-              
-      
-                  };
-             
+                };
             };
+        };
     };
     setTimeout(() => ol.remove(), 11000);
 };
@@ -70,31 +39,18 @@ function createTreeOl(arrIn) {
 
 
 function showTimer({ className, html }) {
-
     let timer = document.createElement('div');
     timer.className = "timer";
-    timer.innerHTML = 'time to remove: ' + i-- ;
+    timer.innerHTML = 'time to remove: ' + i--;
     document.body.append(timer);
-    
-
     setTimeout(() => timer.remove(), 1000);
-    
-    
-
 }
 
 let i = 10;
-let timerId = setInterval(() => {showTimer({});},1000 );
+let timerId = setInterval(() => { showTimer({}); }, 1000);
 
 
 setTimeout(() => clearInterval(timerId), 11000);
 
- 
-
- 
-
-
-
 let arrForExample = ['hello', 'world', 'Kiev', ['wwww', ['wwww', 'errere', 'rtrtr'], 'errere', 'rtrtr'], 'Kharkiv', 'Odessa', 'Lviv'];
 createTreeOl(arrForExample);
-console.log(typeof(arrForExample))
