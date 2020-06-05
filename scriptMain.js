c315424a92cd8e9ad89af204ae5ec55531340e69
SalomonKein <61635747+SalomonKein@users.noreply.github.com>
2020-06-05 15:14:16 +0300

    add finish changes

--- scriptMain.js
+++ scriptMain.js
@@ -1,97 +1,43 @@
-// function createTreeOl(arrIn) {
-
-// item => (typeof(item) == 'object') ?
-// () => createLi(item) :
-// () => `<li>` + item + `</li>`
-// }
+function createUl(arrEl) {
+    return arrEl.map(function(el) {
+        if (Array.isArray(el)) {
+            return `<ul>` + createUl(el) + `</ul>`
+        } else {
+            return `<li>` + el + `</li>`
+        };
+    }).join('');
+};
 
 let createLi = (arr) => {
-    let ul = document.createElement('ul')
-
+    let ol = document.createElement('ol')
     console.log(typeof(arr));
-    ul.innerHTML = arr.map(function(item) {
-        if (typeof(item) == 'object') {
-
-            return createLi(item);
-
+    console.log(arr.map(function(item) {
+        if (Array.isArray(item)) {
+            return `<ul>` + item.map(function(elem) {
+                return `<li>` + elem + `</li>`
+            }) + `</ul>`
         } else {
-
+            return `<li>` + item + `</li>`
+        };
+    }))
+    ol.innerHTML = arr.map(function(item) {
+        if (Array.isArray(item)) {
+            return `<ul>` + createUl(item) + `</ul>`
+        } else {
             return `<li>` + item + `</li>`
         };
     }).join('');
-    console.log(ul);
-    document.body.append(ul);
+    console.log(ol);
+    document.body.append(ol);
+    setTimeout(() => ol.remove(), 11000)
 }
-console.log(createLi);
+
+let arrForExample = ['hello', 'world', 'Kiev', ['wwww', ['111', ['q1', [4, 6, 2, ], 'q2', 'q3'], 'e222', '3333'], 'errere', 'rtrtr'], 'Kharkiv', 'Odessa', 'Lviv'];
 
 
-//     let i = 0;
-//     // let li = document.createElement(`li`);
-//     let newArrey = arrIn.map(item => `<li>` + item + `</li>`);
-
-// container.innerHTML = createTreeText(obj);
-//     let ol = document.createElement(`ol`);
-//     body.append(ol);
-//     console.log(typeof(newArrey[4]));
-//     for (let elem of newArrey) {
-//         // li.innerHtml = elem;
-//         ol.append(elem);
-//     }
-// } //     function createTree(container, obj) {
-// //   
-
-
-// };
-//     if (typeof(elem) == "object") {
-//         let arr = elem;
-//         li = document.createElement('li');
-//         ol.append(li);
-//         let ul = document.createElement(`ul`);
-//         li.append(ul);
-//         for (let elem of arr) {
-//             if (typeof(elem) !== "object") {
-//                 li = document.createElement('li');
-//                 ul.append(li);
-//                 li.append(elem);
-//             }
-//             if (typeof(elem) == "object") {
-//                 let arr = elem;
-//                 li = document.createElement('li');
-//                 ol.append(li);
-//                 let ul = document.createElement(`ul`);
-//                 li.append(ul);
-//             };
-//         };
-//     };
-// };
-// setTimeout(() => ol.remove(), 11000);
-// };
-// let div = document.createElement(`div`);
-// div.className = 'container'
-// body.append(div); createTreeText(elem) +
-let arrForExample = ['hello', 'world', 'Kiev', ['wwww', ['wwww', 'errere', 'rtrtr'], 'errere', 'rtrtr'], 'Kharkiv', 'Odessa', 'Lviv'];
 createLi(arrForExample);
 console.log(arrForExample);
-// function createTree(container, arr) {
-//     container.innerHTML = createTreeText(arr);
-// }
-
-
-// function createTreeText(arr) {
-//     let li = '';
-//     let ul;
-//     let i = 0;
-//     for (let elem of arr) {
-//         li += '<li>' + elem + createTreeText(arr[i++]) + '</li>';
-//     }
-//     if (li) {
-//         ul = '<ul>' + li + '</ul>'
-//     }
-//     return ul || '';
-// }
-
-// createTree(container, arrForExample);
-
+console.log(Array.isArray(arrForExample))
 
 
 function showTimer() {
@@ -101,12 +47,8 @@ function showTimer() {
     document.body.append(timer);
     setTimeout(() => timer.remove(), 1000);
 }
-
 let i = 10;
 let timerId = setInterval(() => { showTimer({}); }, 1000);
 
 
 setTimeout(() => clearInterval(timerId), 11000);
-
-
-// createTreeOl(arrForExample);
